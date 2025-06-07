06b20c7bd852b04c2e2d87fc1c5b33c1aad3004a
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-06-07 14:59:48 +0530

    web duplicate

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/",[StudentController::class ,"home"]);
+Route::get("/",[StudentController::class ,"home"]);
 
 Route::post("/store",[StudentController::class,"store"]);
